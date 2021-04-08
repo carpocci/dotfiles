1d4005f10eb29eda8eca8014d9dd0634e195b625
carpocci <carpocci@gmail.com>
2021-04-08 17:28:32 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-sudo apt install zsh git figlet toilet
+echo "Updating and installing dependencies..."
+sudo apt update
+sudo apt install -y zsh figlet toilet
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+mv ~/.zshrc ~/.zshrc.original
 ln -nfs ~/.dotfiles/zsh/zshrc ~/.zshrc
+
+echo "Changing login shell to zsh..."
+chsh -s $(which zsh)
