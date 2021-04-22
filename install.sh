1e51f1a31dd3056f15c8b0ec512aedba4f5ae1e0
carpocci <carpocci@gmail.com>
2021-04-22 15:47:24 +0200

    added support for different package managers

--- install.sh
+++ install.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 
+managers=("apt" "yum") # supported managers
+dependencies=("zsh" "figlet" "toilet")
+
+find-manager() {
+    for manager in "${managers[@]}"; do
+        which $manager >/dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            echo $manager
+            return 0
+        fi
+    done
+    return 1
+}
+
 echo "Updating and installing dependencies..."
-sudo apt update
-sudo apt install -y zsh figlet toilet
+case $(find-manager) in
+"apt")
+    sudo apt update
+    sudo apt install -y ${dependencies[@]}
+    ;;
+"yum")
+    sudo yum update -y
+    sudo yum install -y ${dependencies[@]}
+    ;;
+*)
+    echo "This system isn't supported yet. Sorry!"
+    echo "Supported package managers: ${managers[@]}"
+    echo "Exiting..."
+    exit 1
+    ;;
+esac
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-mv ~/.zshrc ~/.zshrc.original
+[ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.$(date +%Y-%m-%d-%H:%S)
+
 ln -nfs ~/.dotfiles/zsh/zshrc ~/.zshrc
 
 echo "Changing login shell to zsh..."
